912518058e5afc7f5cc41df941e3bc338fda0190
buhtib <2418531916@qq.com>
2020-08-06 12:16:52 +0800

    add 退出登录

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -4,11 +4,18 @@
 
 		<breadcrumb class="breadcrumb-container" />
 
-		<div class="avatar-container">
-			<div class="avatar-container_header">
-				<img src="~@/assets/img/logo.png" alt srcset draggable="false" />
-				<span>智能桂联</span>
-			</div>
+		<div class="right-menu">
+			<el-dropdown class="avatar-container" trigger="click">
+				<div class="avatar-wrapper">
+					后台管理系统
+					<i class="el-icon-caret-bottom" />
+				</div>
+				<el-dropdown-menu slot="dropdown" class="user-dropdown">
+					<el-dropdown-item @click.native="logout">
+						<span style="display:block;">退出登录</span>
+					</el-dropdown-item>
+				</el-dropdown-menu>
+			</el-dropdown>
 		</div>
 	</div>
 </template>
@@ -21,16 +28,20 @@ import Hamburger from "@/components/Hamburger";
 export default {
 	components: {
 		Breadcrumb,
-		Hamburger
+		Hamburger,
 	},
 	computed: {
-		...mapGetters(["sidebar", "avatar"])
+		...mapGetters(["sidebar", "avatar"]),
 	},
 	methods: {
 		toggleSideBar() {
 			this.$store.dispatch("app/toggleSideBar");
-		}
-	}
+		},
+		async logout() {
+			await this.$store.dispatch("user/logout");
+			this.$router.push(`/login?redirect=${this.$route.fullPath}`);
+		},
+	},
 };
 </script>
 
@@ -76,6 +87,51 @@ export default {
 				width: 70px;
 				height: 20px;
 				object-fit: cover;
+			}
+		}
+	}
+
+	.right-menu {
+		float: right;
+		height: 100%;
+		line-height: 50px;
+
+		&:focus {
+			outline: none;
+		}
+
+		.right-menu-item {
+			display: inline-block;
+			padding: 0 8px;
+			height: 100%;
+			font-size: 18px;
+			color: #5a5e66;
+			vertical-align: text-bottom;
+
+			&.hover-effect {
+				cursor: pointer;
+				transition: background 0.3s;
+
+				&:hover {
+					background: rgba(0, 0, 0, 0.025);
+				}
+			}
+		}
+
+		.avatar-container {
+			margin-right: 30px;
+
+			.avatar-wrapper {
+				margin-top: 5px;
+				position: relative;
+				cursor: pointer;
+				.el-icon-caret-bottom {
+					cursor: pointer;
+					position: absolute;
+					right: -16px;
+					top: 16px;
+					font-size: 12px;
+				}
 			}
 		}
 	}
