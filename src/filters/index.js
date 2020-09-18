8d4f6227dc46c3c2755385f64a595dd57004322b
buhtib <2418531916@qq.com>
2020-09-18 15:32:11 +0800

    add 增加过滤器

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import { date, dateTime } from './dateFormat'
+import noContentFormat from './noContentFormat'
 
 const filters = {
     dateFormat:date,
-    dateTimeFormat:dateTime,
+    dateTimeFormat: dateTime,
+    noContentFormat:noContentFormat
 };
 
 Object.keys(filters).forEach(key => {
---
+++ src/filters/noContentFormat.js
@@ -0,0 +1,7 @@
+export default function (value) {
+    if (!value) {
+        return "-";
+    } else {
+        return value;
+    }
+}
